414d3b90b4bc7faad032ca5715c0ac6c0dc57aaf
peiqianggao <peiqianggao@foxmail.com>
2016-09-05 16:27:03 +0800

    Update Config.java
    commit weixin token

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "pxyYiV7mmKCKw9";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "9b7408236e8349b693de7030b650a2d5"; 
 	
 }
